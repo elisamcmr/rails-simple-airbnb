39d72e5023519823899ca869131dcffa4e5d7a3b
Elisabeth Camara <ecamara.k@gmail.com>
2024-11-13 16:31:43 +0100

    simple airbnb

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,20 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+require 'faker'
+
+# 1. Clean the database 🗑️
+puts 'Cleaning database...'
+Flat.destroy_all
+
+# 2. Create the instances 🏗️
+puts 'Creating flats...'
+10.times do
+  Flat.create!(
+    name: Faker::Address.community,
+    address: Faker::Address.full_address,
+    description: Faker::Lorem.paragraph(sentence_count: 10),
+    price_per_night: rand(50..300),
+    number_of_guests: rand(2..10)
+  )
+end
